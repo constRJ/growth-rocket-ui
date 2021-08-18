8651b4e02bf8e7ef68236096a03604012b8be585
captmjay <commander.mjay23@gmail.com>
2021-08-19 00:11:47 +0800

    project setup

---
+++ src/containers/LandingPageLayout/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const LandingPageLayout = () => {
+  return (
+    <>
+      <h1>Navbar</h1>
+      <h1>SIdebar</h1>
+      <h1>Footer</h1>
+      <button className="btn btn-yellow">This is button</button>
+    </>
+  )
+}
+
+
+export default LandingPageLayout
---
+++ src/pages/LandingPage/index.js
@@ -0,0 +1,12 @@
+import LandingPageLayout from "@containers/LandingPageLayout";
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="container">
+      <LandingPageLayout />
+    </div>
+  )
+}
+
+export default LandingPage
