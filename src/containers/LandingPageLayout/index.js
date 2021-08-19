ea5be7a73d02fa8fe81bf82d542940f7bf319139
captmjay <commander.mjay23@gmail.com>
2021-08-19 08:51:44 +0800

    added navbar

--- src/containers/LandingPageLayout/index.js
+++ src/containers/LandingPageLayout/index.js
@@ -1,13 +1,10 @@
+import BurgerHouseNavbar from "@components/Navbar";
 import React from "react";
-
 
 const LandingPageLayout = () => {
   return (
     <>
-      <h1>Navbar</h1>
-      <h1>SIdebar</h1>
-      <h1>Footer</h1>
-      <button className="btn btn-yellow">This is button</button>
+      <BurgerHouseNavbar />
     </>
   )
 }
