6e1fd1d4d1811c6f3cd20b20e66c86e5f4f71b32
captmjay <commander.mjay23@gmail.com>
2021-08-20 06:09:20 +0800

    added showcase section

--- src/containers/LandingPageLayout/index.js
+++ src/containers/LandingPageLayout/index.js
@@ -1,11 +1,13 @@
 import BurgerHouseBanner from "@components/Banner";
 import BurgerHouseNavbar from "@components/Navbar";
+import BurgerHouseShowcase from "@components/Showcase";
 import React from "react";
 const LandingPageLayout = () => {
   return (
     <>
       <BurgerHouseNavbar />
       <BurgerHouseBanner />
+      <BurgerHouseShowcase />
     </>
   )
 }
