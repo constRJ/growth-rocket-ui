09e69f00b590584dbe75f0a033aa2897523a36bc
captmjay <commander.mjay23@gmail.com>
2021-08-19 12:00:12 +0800

    added banner

--- src/containers/LandingPageLayout/index.js
+++ src/containers/LandingPageLayout/index.js
@@ -1,10 +1,11 @@
+import BurgerHouseBanner from "@components/Banner";
 import BurgerHouseNavbar from "@components/Navbar";
 import React from "react";
-
 const LandingPageLayout = () => {
   return (
     <>
       <BurgerHouseNavbar />
+      <BurgerHouseBanner />
     </>
   )
 }
