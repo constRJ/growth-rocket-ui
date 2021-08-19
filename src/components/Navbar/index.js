13d4ceeb850fd2c177d65182e99ba1d6ba4f0f71
captmjay <commander.mjay23@gmail.com>
2021-08-19 09:54:00 +0800

    update navbar style

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,28 +6,30 @@ const BurgerHouseNavbar = () => {
   return (
     <>
       <nav className="nav">
-        <div className="navbar-brand-container">
-          <a href="/" className="navbar-brand"><img src={BurgerHouseLogo} alt="burger house logo" /></a>
-        </div>
-        <div className="navbar-right-container">
-          <div className="navbar-express">
-            <img src={MotorIcon} alt="express-delivery-icon" className="navbar-express-icon" />
-            <p className="text-b">Express Delivery +1 234 567 890</p>
+        <div className="container custom-container">
+          <div className="navbar-brand-container">
+            <a href="/" className="navbar-brand"><img src={BurgerHouseLogo} alt="burger house logo" /></a>
           </div>
-          <ul className="navbar">
-            <li className="nav-item">
-              <a href="/" className="nav-link">HOME</a>
-            </li>
-            <li className="nav-item">
-              <a href="/" className="nav-link">MENU</a>
-            </li>
-            <li className="nav-item">
-              <a href="/" className="nav-link">OUR STORY</a>
-            </li>
-            <li className="nav-item">
-              <a href="/" className="nav-link">CONTACT US</a>
-            </li>
-          </ul>
+          <div className="navbar-right-container">
+            <div className="navbar-express">
+              <img src={MotorIcon} alt="express-delivery-icon" className="navbar-express-icon" />
+              <p className="text-b">Express Delivery +1 234 567 890</p>
+            </div>
+            <ul className="navbar">
+              <li className="nav-item">
+                <a href="/" className="nav-link">HOME</a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">MENU</a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">OUR STORY</a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">CONTACT US</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
