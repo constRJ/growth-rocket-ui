d2c9fb7513a06831fcc5cbe673e4cb0b5dfb2920
captmjay <commander.mjay23@gmail.com>
2021-08-20 12:38:22 +0800

    add components

---
+++ src/components/Contact/index.js
@@ -0,0 +1,63 @@
+import BurgerLeft from "@assets/images/reservation-image-1.png";
+import Coke from "@assets/images/reservation-image-2.png";
+import BurgerRight from "@assets/images/reservation-image-3.png";
+import { ButtonSoftRed } from "@components/Button";
+import { FormControl } from "@components/Input";
+import React from "react";
+
+const BurgerHouseContact = () => {
+  return (
+    <React.Fragment>
+      <section className="burger-contact-container">
+        <div className="container">
+          <div className="burger-contact-info">
+            <h4 className="text-b text-bebas">RESERVATION</h4>
+            <h2 className="text-b">BOOK YOUR TABLE</h2>
+          </div>
+          <form>
+            <div className="form-container">
+              <div className="form-col-1">
+                <FormControl
+                  type="text"
+                  placeholder="NAME"
+                />
+                <FormControl
+                  type="text"
+                  placeholder="DATE"
+                />
+                <FormControl
+                  type="number"
+                  placeholder="PEOPLE"
+                />
+              </div>
+              <div className="form-col-2">
+                <FormControl
+                  type="email"
+                  placeholder="EMAIL"
+                />
+                <FormControl
+                  type="time"
+                  placeholder="TIME"
+                />
+                <div className="form-group">
+                  <ButtonSoftRed
+                    className="btn btn-orange-red btn-submit"
+                    type="submit"
+                    title="FIND A TABLE"
+                  />
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="contact-img-container">
+          <img src={BurgerLeft} alt="burger-house-reservation-image" />
+          <img src={Coke} alt="burger-house-reservation-image" />
+          <img src={BurgerRight} alt="burger-house-reservation-image" />
+        </div>
+      </section>
+    </React.Fragment>
+  )
+}
+
+export default BurgerHouseContact
