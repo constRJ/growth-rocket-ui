f1692ec2bb57fb6d2c76db8cb5060c202de320c1
captmjay <commander.mjay23@gmail.com>
2021-08-23 22:56:22 +0800

    update product animation

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,40 +1,89 @@
 import Burger1 from "@assets/products/burger-1.png";
 import Burger2 from "@assets/products/burger-2.png";
-import React from "react";
-
+import { motion, useAnimation } from "framer-motion";
+import React, { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 const BurgerHouseProduct = () => {
+  const [ref, inView] = useInView({
+    threshold: .6
+  })
+  const { ref: cardRef, inView: cardRefInView } = useInView({
+    threshold: .8
+  })
+  const animateProductDescInView = useAnimation()
+  const animateProductItemInView = useAnimation()
+
+  useEffect(() => {
+    if (inView) {
+      animateProductDescInView.start({
+        scale: 1,
+        transition: {
+          duration: .2
+        }
+      })
+    } else {
+      animateProductDescInView.start({
+        scale: 0,
+        transition: {
+          duration: .2
+        }
+      })
+    }
+  }, [inView])
+
+  useEffect(() => {
+    if (cardRefInView) {
+      animateProductItemInView.start({
+        scale: 1,
+        transition: {
+          duration: .2
+        }
+      })
+    } else {
+      animateProductItemInView.start({
+        scale: 0,
+        transition: {
+          duration: .2
+        }
+      })
+    }
+  }, [cardRefInView])
   return (
     <React.Fragment>
       <section className="product-container">
-        <div className="product-desc">
-          <h2 className="text-b">CHOOSE & ENJOY</h2>
-          <p className="text-b">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, aperiam dicta? Delectus culpa nulla est magni sed in molestiae eius sit ea ullam non, cumque, dolores fugiat adipisci corrupti reprehenderit.</p>
+        <div className="product-desc" ref={ref}>
+          <motion.h2 className="text-b"
+            animate={animateProductDescInView}
+          >CHOOSE & ENJOY</motion.h2>
+          <motion.p className="text-b"
+            animate={animateProductDescInView}
+          >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, aperiam dicta? Delectus culpa nulla est magni sed in molestiae eius sit ea ullam non, cumque, dolores fugiat adipisci corrupti reprehenderit.</motion.p>
         </div>
-        <div className="products">
-          <div className="product-item">
+        <div className="products" ref={cardRef}>
+          <motion.div className="product-item" animate={animateProductItemInView}>
             <div className="product-img-container"><img src={Burger1} alt="burger-house" /></div>
             <div className="product-item-desc">
               <h3 className="text-b text-bebas">LOREM IPSUM DOLOR</h3>
               <p className="text-b">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo.</p>
               <button className="btn btn-orange-red btn-cta-order">ORDER NOW</button>
             </div>
-          </div>
-          <div className="product-item">
+          </motion.div>
+          <motion.div className="product-item" animate={animateProductItemInView}>
             <div className="product-img-container"><img src={Burger2} alt="burger-house" /></div>
             <div className="product-item-desc">
               <h3 className="text-b text-bebas">LOREM IPSUM DOLOR</h3>
               <p className="text-b">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo.</p>
               <button className="btn btn-orange-red btn-cta-order">ORDER NOW</button>
             </div>
-          </div>
-          <div className="product-item">
+          </motion.div>
+          <motion.div className="product-item" animate={animateProductItemInView}>
             <div className="product-img-container"><img src={Burger1} alt="burger-house" /></div>
             <div className="product-item-desc">
               <h3 className="text-b text-bebas">LOREM IPSUM DOLOR</h3>
               <p className="text-b">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo.</p>
               <button className="btn btn-orange-red btn-cta-order">ORDER NOW</button>
             </div>
-          </div>
+          </motion.div>
         </div>
       </section>
     </React.Fragment>
